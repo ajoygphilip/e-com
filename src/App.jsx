d450c6c10a1022a2614f444bbe587a7ef4d4b6d8
ajoygp <41632101+ajoygp@users.noreply.github.com>
2024-02-20 08:51:05 +0530

    feat: initial set up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+         
+    <>
+    <h2 className="bg-red-300 text-black pt-1 pb-1 text-center">Hello World React Tailwind</h2>
+    </>
+  )
+}
+
+export default App
