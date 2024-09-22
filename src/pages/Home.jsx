f84e85e3081c53d4c181e959d2c77dddf203f287
ajoy <ajoygp551@gmail.com>
2024-09-22 19:16:32 +0530

    feat(home): add search functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
-import ProductListing from '../components/content/productlisting/ProductListing'
+import { useSearchParams } from 'react-router-dom';
+import ProductListing from '../components/content/productlisting/ProductListing';
 
 export default function Home() {
-  const url = 'https://dummyjson.com/products'
-  return <ProductListing url={url} />
+  let url = 'https://dummyjson.com/products';
+
+  let [searchParams] = useSearchParams();
+  const query = searchParams.get('s') || '';
+  const queryString = query === '' ? '' : `/search?q=${query}`;
+
+  let apiUrl = url.concat(queryString);
+  return <ProductListing url={apiUrl} />;
 }
--- src/utils/getProducts.js
+++ src/utils/getProducts.js
@@ -1,6 +1,11 @@
 export default async function getProducts(url, nextPage) {
-  let skip = nextPage > 1 ? `skip=${(nextPage - 1) * 10}` : ''
-  return fetch(`${url}?limit=10&${skip}`)
+  let skip = nextPage > 1 ? `skip=${(nextPage - 1) * 10}` : '';
+  const limit = 'limit=10';
+
+  const separator = url.includes('?') ? '&' : '?';
+  const fullUrl = `${url}${separator}${limit}${skip ? '&' + skip : ''}`;
+
+  return fetch(fullUrl)
     .then(response => response.json())
-    .then(productsData => productsData)
+    .then(productsData => productsData);
 }
